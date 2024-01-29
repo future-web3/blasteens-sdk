8988b9fc83080e5e53170c3e5aa7393c188d95ee
Wendy <xccxccxccb57@gmail.com>
2024-01-29 17:39:27 +1100

    refactor code

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import store from '../src/store';
 import { gameTicketActions }  from './store/modules/gameTicketSlice';
 import { gameLeaderboardActions } from './store/modules/gameLeaderboardSlice';
 
+import { useDispatch as useGameDispatch, useSelector as useGameSelector} from "react-redux";
+
 export { store };
 
 export { gameTicketActions, gameLeaderboardActions };
+
+export { useGameDispatch, useGameSelector };
 
 function createGameSDK(store) {
     return new gameSDK(store);
